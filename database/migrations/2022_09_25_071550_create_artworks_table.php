b1617de6a3568e1780edf7367bd9eaef5520c0f3
Wayne Khoo <wayne.khoo@helloholo.sg>
2022-10-05 15:42:02 +0800

    feat: new tables for backend

---
+++ database/migrations/2022_09_25_071550_create_artworks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtworksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artworks', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('artist_id')
+            ->constrained('users')
+            ->onDelete('no action');
+
+            $table->foreignId('asset_id')
+            ->constrained('assets')
+            ->onDelete('no action');
+
+            $table->integer('votes')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artworks');
+    }
+}
