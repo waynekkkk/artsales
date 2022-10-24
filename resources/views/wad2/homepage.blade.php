f60c9264b90dd78b460752c0884ee0bfecaa8f03
wgqwerty <owg321@gmail.com>
2022-10-24 19:08:03 +0800

    added discover page

--- resources/views/wad2/homepage.blade.php
+++ resources/views/wad2/homepage.blade.php
@@ -127,7 +127,7 @@
             </div>
             <!--see more-->
             <div class="col-3 justify-content-end d-flex pb-3">
-                <a class="btn border-secondary border rounded-pill">Discover more
+                <a class="btn border-secondary border rounded-pill" href="./discover.blade.php">Discover
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                     </svg>
@@ -295,7 +295,7 @@ var museum_collection = {{ Illuminate\Support\Js::from($museum_collections) }};
                     </div>`; 
                 });
             })                   
-            console.log(contentString)
+            // console.log(contentString)
         //  .catch(error => {
         //  // process error object
         //      return error.message;
