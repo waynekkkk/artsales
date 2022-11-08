48817ac6bd68f1c68ac016af26b11a73e740f890
hambalah <kaydon.lim.2021@scis.smu.edu.sg>
2022-11-08 20:04:35 +0800

    feat: added header for exploring galleries

--- resources/views/wad2/explore.blade.php
+++ resources/views/wad2/explore.blade.php
@@ -130,7 +130,7 @@
     <div id="artworks_modal"></div>
 
     <!--card-->
-    <div class="container" style='margin-bottom:175px; margin-top:30px;' id="artworks">
+    <div class="container" style='margin-bottom:80px; margin-top:30px;' id="artworks">
     <!-- display artworks -->
         <!-- <div class="grids" id="artworks"> -->
            <!-- <h1>not working</h1> -->
@@ -140,9 +140,10 @@
     
     <!-- google maps -->
     <div class="container">
-        <div class="my-5">
-            <div class="container d-flex justify-content-center pt-3 mb-3 position-static">
-                <button type="button" class="btn btn-dark rounded-pill me-3" onclick="new_center()">Take me anywhere &nbsp  
+            <div class="container text-center pt-3 mb-3 position-static">
+                <h3>Explore our Physical Galleries!</h3>
+                <br>
+                <button type="button" class="btn btn-dark rounded-pill mb-2 me-3" onclick="new_center()">Take me anywhere &nbsp  
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dice-2" viewBox="0 0 16 16">
                     <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/>
                     <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
@@ -158,8 +159,7 @@
                 <span class="js-odoo s3" id = 's3' style="font-family: monospace;"></span>
             </div>
             
-            <div id="map" class="" style="height: 600px;"></div>
-        </div>
+            <div id="map" class="border border-dark" style="height: 600px;"></div>
     </div>
 
 <!-- do not delete this commented part, it does not work without this -->
