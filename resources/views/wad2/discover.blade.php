8f89f5a6c2ab415c3bf625ac71f28589d86a02af
hambalah <kaydon.lim.2021@scis.smu.edu.sg>
2022-11-12 02:23:04 +0800

    feat: new discover page

---
+++ resources/views/wad2/discover.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<body>
+    
+    {{-- <nav aria-label="Page navigation">
+      <ul class="pagination justify-content-center">
+        <li class="page-item disabled">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+        <li class="page-item active"><a class="page-link" href="#"></a></li>
+        <li class="page-item"><a class="page-link" href="#"></a></li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+    </nav> --}}
+
+</body>
+@endsection
